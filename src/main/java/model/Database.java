dc04d687c475a2e6aac147e1dd7619c0d5532868
Mathieu Ronvel <mathieu.ronvel@movedigital.ch>
2021-12-22 11:07:32 +0100

    Prepare exercise

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -1,6 +1,18 @@
-Given the `Database` interface below ([see src/main/java/model/Database.java](src/main/java/model/Database.java)):
+package model;
 
-```java
+import java.util.List;
+
+/**
+ * Given the following interface, provide an implementation that allows multiple clients to insert / modify / read data
+ * stored in the database. Data will be stored in files, one file per table, in an appropriate format (format is not
+ * of material importance, text based formats like CSV are acceptable).
+ *
+ * <p>Build a server with a REST API that exposes your database implementation, with all its operations. The server can
+ * be, for example, implemented using Spring Boot: https://spring.io/guides/gs/spring-boot/
+ *
+ * <p>Focus on building a maintainable and efficient implementation. Producing a fully working system within the
+ * allotted time is of secondary importance.
+ */
 public interface Database {
     /**
      * Stores the given values in the given table as a new row, returning the ID of the newly inserted row. If the table
@@ -34,18 +46,3 @@ public interface Database {
      */
     List<String> select(String tableName, int rowId);
 }
-```
-
-Provide an implementation that allows multiple clients to insert / modify / read data stored in the database. Data will
-be stored in files, one file per table, in an appropriate format (format is not of material importance, text based
-formats like CSV are acceptable). 
-Note: You can you 3rd party libraries if you like, but are *not* supposed to use relational database.
-
-Build a server with a [REST API](https://en.wikipedia.org/wiki/Representational_state_transfer) that exposes your
-database implementation, with all its operations. The server can be, for example, implemented using Spring Boot:
-https://spring.io/guides/gs/spring-boot/
-
-We would like to use your solution to measure your experience level in various fields of programming.  
-We do not want you to overdo anything to extremes, but we would like to see you showcasing what you know what you care about in programming.  
-This can be anything from being fast, using modern libraries, inventing clever solutions, having an extensive test coverage, or writing something super-performant.  
-In an ideal case your solution would be a mix well reflecting your knowledge and your preferences.
---
+++ src/main/java/model/NotFoundException.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Exception thrown when a table name / row ID combination does not match an existing row.
+ */
+@SuppressWarnings("serial")
+public class NotFoundException extends RuntimeException {
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
